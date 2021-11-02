9f3a197458cc08c88634cfd10c9f0cbdc009bd06
Stephanie Bentley <sc.bentley@yahoo.com>
2021-11-01 21:02:41 -0400

    Add clockwise

--- pi_code.py
+++ pi_code.py
@@ -1,7 +1,23 @@
 from adafruit_servokit import ServoKit
+import time
 
 kit = ServoKit(channels=16)
+motor = 0
 
-for i in range(10):
-    kit.servo[0].angle = i * 10
+rotate_clockwise()
 
+
+
+
+def rotate_clockwise():
+    kit.servo[motor].angle(10)
+    time.sleep(10)
+    kit.servo[motor].angle(20)
+    time.sleep(10)
+    kit.servo[motor].angle(30)
+    time.sleep(10)
+    kit.servo[motor].angle(40)
+    time.sleep(10)
+    kit.servo[motor].angle(50)
+    time.sleep(10)
+    
