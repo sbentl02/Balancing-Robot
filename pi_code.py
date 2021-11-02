9335f916c23bc4ef5caad94622d7bf7ce95151a7
Stephanie Bentley <sc.bentley@yahoo.com>
2021-11-01 21:07:29 -0400

    Motor

--- pi_code.py
+++ pi_code.py
@@ -8,12 +8,16 @@ motor = 0
 
 def rotate_clockwise():
     kit.servo[motor].angle = 10
+    time.sleep(0.01)
     kit.servo[motor].angle = 20
+    time.sleep(0.01)    
     kit.servo[motor].angle = 30
+    time.sleep(0.01)
     kit.servo[motor].angle = 40
+    time.sleep(0.01)
     kit.servo[motor].angle = 50
 
 rotate_clockwise()
 
-time.sleep(1)
+time.sleep(10)
 kit.servo[motor].angle = 0
