be673b9fe11c3a4203d5881d7647e8b685a0e494
Stephanie Bentley <sc.bentley@yahoo.com>
2021-11-02 16:55:58 -0400

    Add logic to turn motor forward/backward

--- pi_code.py
+++ pi_code.py
@@ -8,13 +8,33 @@ STOP = -0.1
 kit.continuous_servo[motor].throttle = STOP
 time.sleep(1)
 
-def rotate_clockwise():
-    kit.continuous_servo[motor].throttle = 1
-    time.sleep(0.054)
+def forward_90():
+    kit.continuous_servo[motor].throttle = -1
+    time.sleep(10)
     kit.continuous_servo[motor].throttle = STOP
+
+def backward_90(): 
+    kit.continuous_servo[motor].throttle = 1
+    time.sleep(10)
+    kit.continuous_servo[motor].throttle = STOP
+
+def rotate_clockwise():
+    #Turn 90 deg
+    forward_90()
+    #Pause
+    time.sleep(10)
+    #Turn back 90 deg
+    backward_90()
+
+def rotate_counterclockwise():
+    #Turn 90 deg
+    backward_90()    
+    #Pause
+    time.sleep(10)
+    #Turn back 90 deg
+    forward_90()
 
 
 rotate_clockwise()
 
 time.sleep(10)
-kit.servo[motor].angle = 0
